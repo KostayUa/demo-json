a4a3c6f3731876fb98d78b9e990a11a4dae4de43
kostay.ua <kostay.ua@gmail.com>
2024-10-10 12:41:22 +0300

    added serialized the company object to json.

--- src/main/java/org/example/demo/json/Main.java
+++ src/main/java/org/example/demo/json/Main.java
@@ -3,6 +3,7 @@ package org.example.demo.json;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.example.demo.json.model.Company;
 import org.example.demo.json.model.PurchaseOrder;
 
 import java.io.IOException;
@@ -16,6 +17,13 @@ public class Main {
             InputStream input = Main.class.getResourceAsStream("/json/purchaseOrder.json");
             PurchaseOrder order = mapper.readValue(input, PurchaseOrder.class);
             System.out.println("Successfully read JSON: " + order);
+
+            final Company company = new Company();
+            company.setName("Company Name");
+            company.setAddress("Company Address");
+            String json = mapper.writeValueAsString(company);
+            System.out.println(json);
+
 
         } catch (JsonMappingException e) {
             System.err.println("Mapping failed JSON: " + e.getMessage());
